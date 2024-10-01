e0d3f7b8dc39d89acb2ce79202764edc2035bc06
Riccardo Lazzari <153769506+Riccalaz@users.noreply.github.com>
2024-10-01 20:20:02 +0200

    Update thread.java

--- thread.java
+++ thread.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class thread extends Thread{
 	private cp contatore;
 	private int npersone;
-	private int stanza;
+	private int stanza;  //attributo che tiene il numero della stanza una volta entrati
 	
 	public thread(cp c){
 		contatore=c;
@@ -24,16 +24,16 @@ public void run() {
 	System.out.println("persone nel gruppo: "+npersone);
 	
 	while(true) {
-		stanza=ran.nextInt(4);
-		contatore.entra(npersone,stanza);
+		stanza=ran.nextInt(4);  //numero casuale di stanza in cui entrerà il gruppo
+		contatore.entra(npersone,stanza); //metodo che aggiunge in numero di persone al contatore corretto
 		try {
-			Thread.sleep(ran.nextInt(1000)+100);
+			Thread.sleep(ran.nextInt(1000)+100); //attesa di un numero casuale di millisecondi
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		contatore.esce(npersone,stanza);
 		try {
-			Thread.sleep(ran.nextInt(1000)+100);
+			Thread.sleep(ran.nextInt(1000)+100);  //attesa di un numero casuale di millisecondi
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
