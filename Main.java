77d8fed06b984d2eb14228b40b8a94a3de0d7fe8
Riccardo Lazzari <153769506+Riccalaz@users.noreply.github.com>
2024-10-01 20:16:37 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,8 +9,8 @@ public class Main {
 		
 		System.out.print("inserisci n di gruppi: ");
 		
-		int T = myObj.nextInt();
-		thread[] th=new thread[T];
+		int T = myObj.nextInt(); //inserimento numero di thread
+		thread[] th=new thread[T]; //arrey di thread
 		cp c=new cp();
 		
 		for(int i=0;i<T;i++){
@@ -19,7 +19,7 @@ public class Main {
 		}
 		
 		while(true) {
-			System.out.println("pesone in discoteca: 1)"+c.getc(0)+" 2)"+c.getc(1)+" 3)"+c.getc(2)+" 4)"+c.getc(3));
+			System.out.println("pesone in discoteca: 1)"+c.getc(0)+" 2)"+c.getc(1)+" 3)"+c.getc(2)+" 4)"+c.getc(3)); //stmpo a schermo il numero di persone presente in ogni stanza
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
